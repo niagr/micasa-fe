6b36595668086b4f114eb3c6d7e99df56615077f
Nishant George Agrwal <nishant.george@hashedin.com>
2017-08-08 03:41:12 +0530

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {test: /\.tsx?$/, use: 'awesome-typescript-loader'}
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin()]
+}
